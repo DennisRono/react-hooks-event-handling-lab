6dd15d10975dd6f1af131b687ff9f4385e4db1c6
DennisRono <dennisrkibet@gmail.com>
2024-07-31 14:32:18 +0300

    completed events lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,17 @@
 // Code Keypad Component Here
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  return (
+    <div>
+      <input
+        type="password"
+        placeholder="Enter password"
+        onChange={(e) => {
+          console.log('Entering password...')
+        }}
+      />
+    </div>
+  )
 }
 
-export default Keypad;
+export default Keypad
